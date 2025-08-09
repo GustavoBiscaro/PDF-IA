2420ae4de2e3db59a39e3d5f75061597b5917bcd
The Ghostavo <57949820+GustavoBiscaro@users.noreply.github.com>
2025-08-09 13:17:39 -0300

    estruturando o chat

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from langchain.memory import ConversationBufferMemory
 from pathlib import Path
 import time
 
@@ -6,6 +7,12 @@ PASTA_ARQUIVOS = Path(__file__).parent / 'files'
 
 def cria_chain_conversa():
   st.session_state['chain'] = True
+
+  memory = ConversationBufferMemory(return_messages=True)
+  memory.chat_memory.add_user_message('Olá')
+  memory.chat_memory.add_ai_message('Olá, eu sou o Chat IA Pro da Univesp!')
+  st.session_state['memory'] = memory
+  
   time.sleep(1)
   pass
 
@@ -31,13 +38,48 @@ def sidebar():
       cria_chain_conversa()
       st.rerun()
       
+def chat_window():
+  st.header('🤖 Bem-vindo ao Chat IA Pro da Univesp', divider=True)
 
+  if not 'chain' in st.session_state:
+    st.error('Faça o upload de PDFs para iniciar!')
+    st.stop()
+
+  # chain = st.session_state['chain']
+  # memory = chain.memory
+
+  memory = st.session_state['memory']
+  messages = memory.load_memory_variables({})['history']
+
+  container = st.container()
+  for message in messages:
+    chat = container.chat_message(message.type)
+    chat.markdown(message.content)
+
+  new_message = st.chat_input('Inicie uma nova conversa...')
+  if new_message:
+    chat = container.chat_message('human')
+    chat.markdown(new_message)
+    chat = container.chat_message('ai')
+    chat.markdown('Gerando sua resposta')
+
+    time.sleep(2)
+    memory.chat_memory.add_user_message(new_message)
+    memory.chat_memory.add_ai_message('Olá, eu sou o Chat IA Pro da Univesp! O que gostaria de saber?')
+    st.rerun()
+
+    
+    
+  
+  
+
+    
 
 
 def main():
   with st.sidebar:
     sidebar()
-  pass
+  chat_window()
 
 if __name__ == '__main__':
   main()
